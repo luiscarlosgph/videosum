3bfc196a38195c0d47730431aeab96f1a8287879
Luis C. Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2023-02-20 15:49:52 +0000

    The method get_latent_feature_vector() now supports batches.

--- src/uid.py
+++ src/uid.py
@@ -67,28 +67,39 @@ class FrechetInceptionDistance():
     
     def _get_latent_features(self, im):
         """
-        @brief  Given an image, this method computes a 1D feature vector.
+        @brief  Given a BGR image, this method computes a 1D feature vector.
 
         @param[in]  im  Numpy/OpenCV BGR image, shape (H, W, 3),
-                        dtype = np.uint8.
+                        dtype = np.uint8. 
+                        Could also be a batch of images with 
+                        shape (B, H, W, 3).
         
         @returns a Numpy ndarray of 2048 channels.
         """
+        single_image = True if len(im.shape) == 3 else False
+
         # Convert the image to RGB in range [0, 1]
         im = im[...,::-1].copy().astype(np.float32) / 255.
 
         # Convert image from Numpy to Pytorch and upload it to GPU
         im = torch.from_numpy(im).to(self.device)
 
-        # Convert image to shape (1, 3, H, W)
-        im = im.permute(2, 0, 1).unsqueeze(0)
+        # Convert image to shape (B, 3, H, W)
+        if single_image:
+            im = im.permute(2, 0, 1).unsqueeze(0)
+        else:
+            im = im.permute(0, 3, 1, 2)
 
         # Run inference on the image and get a vector of shape (2048,)
         with torch.no_grad():
-            output = self.model(im)[0].flatten().cpu().detach().numpy()
+            if single_image:
+                output = self.model(im)[0].flatten().cpu().detach().numpy()
+            else:
+                bs, chan, _, _ = im.shape
+                output = torch.squeeze(self.model(im)[0]).cpu().detach().numpy()
 
         return output
-    
+
     def get_latent_feature_vector(self, im):
         assert(self.mode == 'vector')
         return self._get_latent_features(im)
